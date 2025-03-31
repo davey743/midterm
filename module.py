0af8f09643531bad207ab6f18ff6f5c47d328620
davey743 <dh43529p@pace.edu>
2025-03-31 17:35:20 -0400

    module

---
+++ module.py
@@ -0,0 +1,14 @@
+import random
+
+def get_computer_choice():
+    return random.choice(["rock", "paper", "scissors"])
+
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (user_choice == "rock" and computer_choice == "scissors") or \
+         (user_choice == "paper" and computer_choice == "rock") or \
+         (user_choice == "scissors" and computer_choice == "paper"):
+        return "You win!"
+    else:
+        return "Computer wins!"
